f9cff10aa65dfb913d096ae0d81c2ae5410cb347
Stefan <stefan.reinhold@chello.at>
2017-04-11 18:59:53 +0200

    Working marching cube

--- Rastertek/GeometryShader.hlsl
+++ Rastertek/GeometryShader.hlsl
@@ -4,7 +4,7 @@ struct GSInput
 	float4 color : COLOR;
 };
 
-struct PixelInputType
+struct GSOuput
 {
 	float4 position : SV_POSITION;
 	float4 color : COLOR0;
@@ -34,7 +34,7 @@ Texture2D<int> tritableTex : register(t1);
 /*
 Multiplies the position by the world, view and projection matrix
 */
-float4 getProjectionPos(float4 position)
+float4 getProjectionPosition(float4 position)
 {
     position = mul(position, worldMatrix);
     position = mul(position, viewMatrix);
@@ -42,38 +42,100 @@ float4 getProjectionPos(float4 position)
     return position;
 }
 
-[maxvertexcount(3)]
+float3 vertexInterpolation(float isoLevel, float3 v0, float l0, float3 v1, float l1)
+{
+    float lerper = (isoLevel - l0) / (l1 - l0);
+    return lerp(v0, v1, lerper);
+}
+
+
+/*
+Gets a value from the look-up table
+*/
+int checkTriangleLUT(int i, int j)
+{
+    if (i >= 256 || j >= 16)
+    {
+        return -1;
+    }
+
+    return tritableTex.Load(int3(j, i, 0));
+}
+
+[maxvertexcount(18)]
 void main(
 	point GSInput input[1] : SV_POSITION, 
-	inout TriangleStream<PixelInputType> output
+	inout TriangleStream<GSOuput> output
 )
 {
-	PixelInputType element;
     float offset = 0.02f;
+    float isolevel = 0.50f;
 
-    float4 position = getProjectionPos(input[0].position);
-    float density = densityTex.SampleLevel(samplerPoint, (input[0].position.xyz + 1) / 2.0f, 0);
-    float4 color = float4(density, density, 0.1f, 1.0f);
+    float4 position = input[0].position;
+    float4 color = input[0].color;
+
+    float3 cubePoses[8];
+    float cubeVals[8];
+    for (int i = 0; i < 8; i++)
+    {
+        cubePoses[i] = position.xyz + decal[i].xyz;
+        cubeVals[i] = densityTex.SampleLevel(samplerPoint, (cubePoses[i] + 1.0f) / 2.0f, 0);
+    }
+
+    int cubeindex = 0;
+    cubeindex = int(cubeVals[0] < isolevel);
+    cubeindex += int(cubeVals[1] < isolevel) * 2;
+    cubeindex += int(cubeVals[2] < isolevel) * 4;
+    cubeindex += int(cubeVals[3] < isolevel) * 8;
+    cubeindex += int(cubeVals[4] < isolevel) * 16;
+    cubeindex += int(cubeVals[5] < isolevel) * 32;
+    cubeindex += int(cubeVals[6] < isolevel) * 64;
+    cubeindex += int(cubeVals[7] < isolevel) * 128;
+
+    if (cubeindex != 0 && cubeindex != 255)
+    {
     
-    //Triangle Begin
-    element.position = position;
-    element.position.x -= offset;
-    element.position.y -= offset;
-    element.color = color;
-	output.Append(element);
+        float3 vertlist[12];
 
-    element.position = position;
-    element.position.y += offset;
-    element.color = color;
-    output.Append(element);
+		//Find the vertices where the surface intersects the cube
+        vertlist[0] = vertexInterpolation(isolevel, cubePoses[0], cubeVals[0], cubePoses[1], cubeVals[1]);
+        vertlist[1] = vertexInterpolation(isolevel, cubePoses[1], cubeVals[1], cubePoses[2], cubeVals[2]);
+        vertlist[2] = vertexInterpolation(isolevel, cubePoses[2], cubeVals[2], cubePoses[3], cubeVals[3]);
+        vertlist[3] = vertexInterpolation(isolevel, cubePoses[3], cubeVals[3], cubePoses[0], cubeVals[0]);
+        vertlist[4] = vertexInterpolation(isolevel, cubePoses[4], cubeVals[4], cubePoses[5], cubeVals[5]);
+        vertlist[5] = vertexInterpolation(isolevel, cubePoses[5], cubeVals[5], cubePoses[6], cubeVals[6]);
+        vertlist[6] = vertexInterpolation(isolevel, cubePoses[6], cubeVals[6], cubePoses[7], cubeVals[7]);
+        vertlist[7] = vertexInterpolation(isolevel, cubePoses[7], cubeVals[7], cubePoses[4], cubeVals[4]);
+        vertlist[8] = vertexInterpolation(isolevel, cubePoses[0], cubeVals[0], cubePoses[4], cubeVals[4]);
+        vertlist[9] = vertexInterpolation(isolevel, cubePoses[1], cubeVals[1], cubePoses[5], cubeVals[5]);
+        vertlist[10] = vertexInterpolation(isolevel, cubePoses[2], cubeVals[2], cubePoses[6], cubeVals[6]);
+        vertlist[11] = vertexInterpolation(isolevel, cubePoses[3], cubeVals[3], cubePoses[7], cubeVals[7]);
 
-    element.position = position;
-    element.position.x += offset;
-    element.position.y -= offset;
-    element.color = color;
-    output.Append(element);
-    //Triangle End
+        GSOuput v0;
+        GSOuput v1;
+        GSOuput v2;
 
-    output.RestartStrip();
-	
+        float3 vector1;
+        float3 vector2;
+        for (int i = 0; checkTriangleLUT(cubeindex, i) != -1; i += 3)
+        {
+			//Add vertices to the output stream
+            v0.position = float4(vertlist[checkTriangleLUT(cubeindex, i + 0)], 1);
+            v0.position = getProjectionPosition(v0.position);
+            v0.color = color;
+			
+            v1.position = float4(vertlist[checkTriangleLUT(cubeindex, i + 1)], 1);
+            v1.position = getProjectionPosition(v1.position);
+            v1.color = color;
+				
+            v2.position = float4(vertlist[checkTriangleLUT(cubeindex, i + 2)], 1);
+            v2.position = getProjectionPosition(v2.position);
+            v2.color = color;
+
+            output.Append(v0);
+            output.Append(v1);
+            output.Append(v2);
+            output.RestartStrip();
+        }
+    }
 }
